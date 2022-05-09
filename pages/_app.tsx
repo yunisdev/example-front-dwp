dbe9753d246e87ffabb539efb6e13658832a81fe
Yunis Huseynzade <yunisdev.04@gmail.com>
2022-05-09 20:06:51 +0400

    init

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,34 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import '../styles/globals.css';
+import type { AppProps } from 'next/app';
+import {
+	ContactlessProvider,
+	Container,
+	createTheme,
+	Navbar,
+	NavLink,
+	Button,
+} from 'contactless';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+	const theme = createTheme({ colors: {} });
+	return (
+		<ContactlessProvider theme={theme}>
+			<>
+				<Navbar
+					brand={<b>Digital PE</b>}
+					style={{ background: 'white' }}
+				>
+					<Button style={{marginLeft:8}} color="purple">Platform ▼</Button>
+					<Button style={{marginLeft:8}} color="purple">Status ▼</Button>
+					<Button style={{marginLeft:8}} color="purple">Well Name ▼</Button>
+					<Button style={{marginLeft:8}} color="purple">Well Type ▼</Button>
+				</Navbar>
+				<Container style={{ padding: 0 }}>
+					<Component {...pageProps} />
+				</Container>
+			</>
+		</ContactlessProvider>
+	);
 }
 
-export default MyApp
+export default MyApp;
